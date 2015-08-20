4027896e7d024acf6146fa5caef807710f6b4528
sbuberl <sbuberl@gmail.com>
2015-08-19 20:46:34 -0400

    Trying out Travis-CI with PHPUnit

---
+++ tests/fSQLTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class fSQLTest extends PHPUnit_Framework_TestCase
+{
+    var $fsql;
+
+    function setUp()
+    {
+        $this->fsql =& new fSQLEnvironment();
+        mkdir(".tmp");
+    }
+
+    function tearDown()
+    {
+        $this->close();
+        unset($this->fsql);
+        rmdir (".tmp");
+    }
+
+    function testDefineDB()
+    {
+        $this->assertTrue($this->define_db("db1", ".tmp"));
+    }
+}
+
+?>
