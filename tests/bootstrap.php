28d65ee427ddf2a98020ee1c4a676ddd0ae4ed36
sbuberl <sbuberl@gmail.com>
2016-04-08 23:35:42 -0400

    Removed opcache population.
    Made most tests slower.  Make take another look in future.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,3 @@
 <?php
 
 require 'vendor/autoload.php';
-
-$cacheFunction = null;
-
-if (extension_loaded('Zend OpCache') && ini_get('opcache.enable_cli')) {
-    if (function_exists('opcache_compile_file')) {
-        $cacheFunction = 'opcache_compile_file';
-    }
-} elseif (extension_loaded('apc') && ini_get('apc.enable_cli')) {
-    $cacheFunction = 'apc_compile_file';
-}
-
-if ($cacheFunction !== null) {
-    $directory = dirname(__DIR__).'/src';
-    $files = new FilesystemIterator($directory);
-    foreach ($files as $entry) {
-        if ($entry->getExtension() === 'php') {
-            $cacheFunction($entry->getPathname());
-        }
-    }
-}
