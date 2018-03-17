36928d7e7399bc9dafa0a087d879b25746281ab7
sbuberl <sbuberl@gmail.com>
2018-03-17 19:59:59 -0400

    Adds updates tests for collisions and ignore

--- tests/SQL/UpdateTest.php
+++ tests/SQL/UpdateTest.php
@@ -15,6 +15,11 @@ class UpdateTest extends BaseTest
         'field_data' => ['type' => 's', 'auto' => 0, 'default' => null, 'key' => 'n', 'null' => 1, 'restraint' => []],
     ];
 
+    private static $optionsWithKey = [
+        'field_name' => ['type' => 's', 'auto' => 0, 'default' => null, 'key' => 'p', 'null' => 1, 'restraint' => []],
+        'field_data' => ['type' => 's', 'auto' => 0, 'default' => null, 'key' => 'n', 'null' => 1, 'restraint' => []],
+    ];
+
     public function setUp()
     {
         parent::setUp();
@@ -106,5 +111,47 @@ class UpdateTest extends BaseTest
         ];
         $this->assertEquals($expected, $table->getEntries());
         $this->assertEquals(1, $this->fsql->affected_rows());
+    }
+
+    public function testCollisionIgnore()
+    {
+        $rows = [
+            ['graph_data_amount_total', '---time=1234567890---total=15'],
+            ['graph_data_range_recv', '---time=1234567890---total=5'],
+            ['graph_data_range_sent', '---time=1234567890---total=15'],
+            ['max_active_peers', '5'],
+        ];
+        $table = CachedTable::create($this->fsql->current_schema(), 'options', self::$optionsWithKey);
+        $cursor = $table->getWriteCursor();
+        foreach($rows as $row) {
+            $cursor->appendRow($row);
+        }
+        $table->commit();
+        $result = $this->fsql->query("UPDATE IGNORE options SET field_name = 'max_active_peers' WHERE field_name = 'graph_data_range_recv'" );
+        $this->assertTrue($result);
+
+        $this->assertEquals($rows, $table->getEntries());
+        $this->assertEquals(0, $this->fsql->affected_rows());
+    }
+
+    public function testCollision()
+    {
+        $rows = [
+            ['graph_data_amount_total', '---time=1234567890---total=15'],
+            ['graph_data_range_recv', '---time=1234567890---total=5'],
+            ['graph_data_range_sent', '---time=1234567890---total=15'],
+            ['max_active_peers', '5'],
+        ];
+        $table = CachedTable::create($this->fsql->current_schema(), 'options', self::$optionsWithKey);
+        $cursor = $table->getWriteCursor();
+        foreach($rows as $row) {
+            $cursor->appendRow($row);
+        }
+        $table->commit();
+        $result = $this->fsql->query("UPDATE options SET field_name = 'max_active_peers' WHERE field_name = 'graph_data_range_recv'" );
+        $this->assertFalse($result);
+
+        $this->assertEquals($rows, $table->getEntries());
+        $this->assertEquals(0, $this->fsql->affected_rows());
     }
 }
