57f8dd72f8d5580c54357624893ca6958fc72bc8
sbuberl <sbuberl@gmail.com>
2018-05-17 21:31:07 -0400

    Added key no collision for Update test

--- tests/SQL/UpdateTest.php
+++ tests/SQL/UpdateTest.php
@@ -118,6 +118,35 @@ class UpdateTest extends BaseTest
         $this->assertEquals(1, $this->fsql->affected_rows());
     }
 
+    public function testKeyNoCollision()
+    {
+        $rows = [
+            ['graph_data_amount_total', '---time=1234567890---total=15'],
+            ['graph_data_range_recv', '---time=1234567890---total=5'],
+            ['graph_data_range_sent', '---time=1234567890---total=15'],
+            ['max_active_peers', '5'],
+        ];
+        $table = CachedTable::create($this->fsql->current_schema(), 'options', self::$optionsWithKey);
+        $cursor = $table->getWriteCursor();
+        foreach($rows as $row) {
+            $cursor->appendRow($row);
+        }
+        $table->commit();
+        $result = $this->fsql->query("UPDATE options SET field_name = 'max_total_peers' WHERE field_name = 'graph_data_range_recv'" );
+        var_dump($this->fsql->error());
+        $this->assertTrue($result);
+
+        $expected = [
+            ['graph_data_amount_total', '---time=1234567890---total=15'],
+            ['max_total_peers', '---time=1234567890---total=5'],
+            ['graph_data_range_sent', '---time=1234567890---total=15'],
+            ['max_active_peers', '5'],
+        ];
+
+        $this->assertEquals($expected, $table->getEntries());
+        $this->assertEquals(1, $this->fsql->affected_rows());
+    }
+
     public function testCollisionIgnore()
     {
         $rows = [
